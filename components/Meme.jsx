ff35008768d8a9b4468dc22942d1e4088dee5163
barath <barath11raju@gmail.com>
2023-06-23 00:08:23 +0530

    done with the basic concepts and also this project

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,35 +1,82 @@
-import React, {useState}from "react";
-import memesData from "../src/memesData";
+import React, { useState, useEffect } from "react";
+
 
 export default function Meme() {
-  const [memeImg, setImg] = useState(
-    {
-      "topText": "",
-      "bottomText": "",
-      "randomImage": ""
+ 
+  const [memeImg, setMemeImg] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = React.useState([])
+  /**
+useEffect takes a function as its parameter. If that function
+returns something, it needs to be a cleanup function. Otherwise,
+it should return nothing. If we make it an async function, it
+automatically retuns a promise instead of a function or nothing.
+Therefore, if you want to use async operations inside of useEffect,
+you need to define the function separately inside of the callback
+function, as seen below:
+*/
+  useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes")
+      const data = await res.json()
+      setAllMemes(data.data.memes)
     }
-  );
-  const [allMemeImages, setMemes] = useState(memesData);
-  const randomMeme = () => {
-    const memes = memesData.data.memes;
-    const randomItem = Math.floor(Math.random() * memes.length);
-    let url = memes[randomItem].url;
-    setImg(prevState => ({
-      ...memeImg,
-      "randomImage": url
-    }))
+  getMeme()
+  }, [])
+  
+  function handleChange(){
+      const{name, value} = event.target;
+     setMemeImg(prev =>({
+      ...prev,
+      [name]: value
+     }))
+  }
+
+  function randomMeme() {
+    const randomItem = Math.floor(Math.random() * allMemes.length);
+    let url = allMemes[randomItem].url;
+    setMemeImg(prevState => ({
+      ...prevState,
+      randomImage: url
+    }));
   };
+
+  
+
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
-        <button onClick={randomMeme} className="form--button">
+        <input 
+              type="text"
+              placeholder="Top text" 
+              className="form--input"
+              name="topText"
+              onChange={handleChange}
+              value={memeImg.topText}
+        />
+        <input 
+              type="text" 
+              placeholder="Bottom text" 
+              className="form--input"
+              name="bottomText"
+              onChange={handleChange}
+              value={memeImg.bottomText}
+        />
+        <button 
+              onClick={randomMeme} 
+              className="form--button"
+        >
           Get a new meme image 🖼
         </button>
       </div>
-      <div>
+      <div className="meme">
         <img src={memeImg.randomImage} className="meme-img" />
+        <h3 className="meme--text top">{memeImg.topText}</h3>
+        <h3 className="meme--text bottom">{memeImg.bottomText}</h3>
       </div>
     </main>
   );
